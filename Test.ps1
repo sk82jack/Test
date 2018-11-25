8591ed8df13535837ca2d26a3c2ba67879e5ca36
sk82jack <sk82jack@users.noreply.github.com>
2018-11-25 21:49:12 +0000

    commit

--- Test.ps1
+++ Test.ps1
@@ -1,13 +1,13 @@
-$ENV:BUILD_NAME #| Out-File -FilePath .\BUILDVERSION.md -Force
+$ENV:BUILD_NAME
 
 if ($ENV:TEST1) {
-    "This variable is {}" -f $ENV:TEST1
+    'This variable is {0}' -f $ENV:TEST1
 }
 else {
     Write-Error 'Failed on variable $ENV:TEST1'
 }
 if ($ENV:TEST2) {
-    "This variable is {}" -f $ENV:TEST1
+    "This variable is {0}" -f $ENV:TEST1
 }
 else {
     Write-Error 'Failed on variable $ENV:TEST2'
