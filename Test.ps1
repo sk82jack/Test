00407757fd343c2ce537844ca637505cde46293d
sk82jack <sk82jack@users.noreply.github.com>
2018-11-25 22:23:49 +0000

    fix bug

--- Test.ps1
+++ Test.ps1
@@ -3,27 +3,27 @@ $ENV:BUILD_NAME | Out-File -FilePath .\BUILDVERSION.md -Force
 Get-Content -Path .\BUILDVERSION.md
 
 if ($ENV:TEST1) {
-    'This variable is {0}' -f $ENV:TEST1
+    'TEST1 variable is {0}' -f $ENV:TEST1
 }
 else {
     Write-Error 'Failed on variable $ENV:TEST1'
 }
 
 if ($ENV:TEST2 -eq '***value2***') {
-    "This variable is {0}" -f $ENV:TEST2
+    "TEST2 variable is {0}" -f $ENV:TEST2
 }
 else {
     Write-Error 'Failed on variable $ENV:TEST2'
 }
 
 if ($ENV:TEST3) {
-    "This variable is {0}" -f $ENV:TEST3
+    "TEST3 variable is {0}" -f $ENV:TEST3
 }
 else {
     Write-Error 'Failed on variable $ENV:TEST3'
 }
 if ($ENV:GITHUB_PAT) {
-    "This variable is {0}" - $ENV:GITHUB_PAT
+    "GITHUB_PAT variable is {0}" -f $ENV:GITHUB_PAT
 }
 else {
     Write-Error 'Failed on $ENV:GITHUB_PAT'
