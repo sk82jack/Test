3d46d1b419f9744045abb10ba854ce617fb8efcb
sk82jack <sk82jack@users.noreply.github.com>
2018-11-25 22:14:18 +0000

    ANother commit

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,5 @@
 $ENV:BUILD_NAME | Out-File -FilePath .\BUILDVERSION.md -Force
-"Contents of BUILDVERSION.md:`n"
+"Contents of BUILDVERSION.md:"
 Get-Content -Path .\BUILDVERSION.md
 
 if ($ENV:TEST1) {
@@ -9,7 +9,7 @@ else {
     Write-Error 'Failed on variable $ENV:TEST1'
 }
 
-if ($ENV:TEST2) {
+if ($ENV:TEST2 -eq '***value2***') {
     "This variable is {0}" -f $ENV:TEST2
 }
 else {
