84414362a2dd325091a731037b91d73246f87414
sk82jack <sk82jack@users.noreply.github.com>
2018-11-25 22:08:16 +0000

    Few changes

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,6 @@
-$ENV:BUILD_NAME
+$ENV:BUILD_NAME | Out-File -FilePath .\BUILDVERSION.md -Force
+"Contents of BUILDVERSION.md:`n"
+Get-Content -Path .\BUILVERSION.md
 
 if ($ENV:TEST1) {
     'This variable is {0}' -f $ENV:TEST1
@@ -6,15 +8,17 @@ if ($ENV:TEST1) {
 else {
     Write-Error 'Failed on variable $ENV:TEST1'
 }
+
 if ($ENV:TEST2) {
-    "This variable is {0}" -f $ENV:TEST1
+    "This variable is {0}" -f $ENV:TEST2
 }
 else {
     Write-Error 'Failed on variable $ENV:TEST2'
 }
+
 if ($ENV:TEST3) {
     "This variable is {0}" -f $ENV:TEST3
 }
 else {
-    Write-Error 'Failed on variable $ENV:TEST2'
+    Write-Error 'Failed on variable $ENV:TEST3'
 }
