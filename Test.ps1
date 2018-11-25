72bc37e343b86109f7c62d7f87f75affd5d71260
sk82jack <sk82jack@users.noreply.github.com>
2018-11-25 21:36:29 +0000

    Add some azure variable tests

---
+++ Test.ps1
@@ -0,0 +1,12 @@
+if ($ENV:TEST1) {
+    "This variable is {}" -f $ENV:TEST1
+}
+else {
+    Write-Error 'Failed on variable $ENV:TEST1'
+}
+if ($ENV:TEST2) {
+    "This variable is {}" -f $ENV:TEST1
+}
+else {
+    Write-Error 'Failed on variable $ENV:TEST2'
+}
