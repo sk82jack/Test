1aa667a0774e598b217c58c59879d5c981790b28
sk82jack <sk82jack@users.noreply.github.com>
2019-10-26 14:14:47 +0100

    add GitLab shit

---
+++ build.ps1
@@ -0,0 +1,16 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $Task,
+
+    [Parameter()]
+    [switch]
+    $Bootstrap
+)
+
+"This is running the $Task task"
+
+if ($Bootstrap) {
+    "Bootstrap has also been specified"
+}
